32e9f953a020d122d30a99798fafd9ec196937c8
Kawsar Rashik <kawsar.rashik@gmail.com>
2024-05-25 00:47:53 +0600

    Worked on Region Controller.. Repository pattern.

---
+++ NZWalks.API/Repositories/SQLRegionRepository.cs
@@ -0,0 +1,19 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class SQLRegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDBContext dbContext;
+        public SQLRegionRepository(NZWalksDBContext dbContext)
+        {
+             this.dbContext = dbContext;
+        }
+
+        public async Task<List<Region>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
